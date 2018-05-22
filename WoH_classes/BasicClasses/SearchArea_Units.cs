a2376cb8dc73552823debe060f01260bec529710
Dzuke <Dzuke911@gmail.com>
2018-05-22 13:21:10 +0300

    SearchArea Added

---
+++ WoH_classes/BasicClasses/SearchArea_Units.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WoH_classes.Interfaces;
+
+namespace WoH_classes.BasicClasses
+{
+    class SearchArea_Units
+    {
+        private readonly int _radius;
+        private readonly int _resultNumber;
+        private readonly List<ICondition> _conditions;
+
+        public SearchArea_Units(int radius, int resultNumber, List<ICondition> conditions)
+        {
+            _radius = radius;
+            _resultNumber = resultNumber;
+            _conditions = conditions;
+        }
+    }
+}
---
+++ WoH_classes/Interfaces/ISearchArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WoH_classes.BasicClasses;
+
+namespace WoH_classes.Interfaces
+{
+    public interface ISearchArea
+    {
+        List<Unit> Search();
+    }
+}
