eea1388ae4c1554fc277601a1da6e44183ced332
Dzuke <Dzuke911@gmail.com>
2018-06-17 02:06:30 +0300

    react hexagons

--- WohClassesVisualiser/wwwroot/js/Battlefield.jsx
+++ WohClassesVisualiser/wwwroot/js/Battlefield.jsx
@@ -24,16 +24,49 @@
     }
 
     render() {
+
         let int = this.state.map.LenghtX;
         let hexes = this.state.map.Hexes;
-        console.log(hexes);
-        let X;
+        console.log(hexes);   
+
+        let xRad = 50;
+        let yRad = xRad * 0.866;
+
+        let x1 = 0 - xRad / 2;
+        let y1 = 0 - yRad;
+        let x2 = xRad/2;
+        let y2 = y1;
+        let x3 = xRad;
+        let y3 = 0;
+        let x4 = x2;
+        let y4 = yRad;
+        let x5 = x1;
+        let y5 = y4;
+        let x6 = 0 - xRad;
+        let y6 = y3;
+
         if (hexes != undefined) {
-            X = hexes[10].X;
+
+            let rows = [];
+
+            let xOffset = xRad * (1.5 * this.state.map.OffsetX + 2);
+            let yOffset = yRad * (2 * this.state.map.OffsetY + 2);
+
+            for (let i = 0; i < hexes.length; i++) {
+                rows.push(<HexFrame key={hexes[i].ID} xRad={xRad} yRad={yRad} xCenter={xOffset} yCenter={yOffset} xCoord={hexes[i].X} yCoord={hexes[i].Y} x1={x1} y1={y1} x2={x2} y2={y2} x3={x3} y3={y3} x4={x4} y4={y4} x5={x5} y5={y5} x6={x6} y6={y6} />);
+            }
+
+            console.log(rows);
+
+            return <svg height="720" width="1020" style={{ position: 'absolute', top: '0px', left: '0px' }}>
+                {rows}
+            </svg>;
         }
-        return <div>
-            <p>{int}</p>
-            <p>{X}</p>
-        </div>;
+        else {
+            return <div>
+                <p>{int}</p>
+            </div>;
+        }
+
     }
 }
---
+++ WohClassesVisualiser/wwwroot/js/HexFrame.jsx
@@ -0,0 +1,41 @@
+﻿class HexFrame extends React.Component {
+    render() {
+        let xCoord = this.props.xCoord;
+        let yCoord = this.props.yCoord;
+        let xRad = this.props.xRad;
+        let yRad = this.props.yRad;
+        let xCenter = this.props.xCenter;
+        let yCenter = this.props.yCenter;
+
+        let xOffset = 0;
+        let yOffset = 0;
+
+        if (xCoord % 2 == 0) {
+            xOffset = xCenter + xRad * (xCoord * 1.5);
+            yOffset = yCenter + yCoord * yRad * 2;
+        }
+        else {
+            xOffset = xCenter + xRad * (xCoord * 1.5);
+            yOffset = yCenter + yRad * yCoord * 2 + yRad;
+        }
+
+        x1 = xOffset + this.props.x1;
+        y1 = yOffset + this.props.y1;
+        x2 = xOffset + this.props.x2;
+        y2 = yOffset + this.props.y2;
+        x3 = xOffset + this.props.x3;
+        y3 = yOffset + this.props.y3;
+        x4 = xOffset + this.props.x4;
+        y4 = yOffset + this.props.y4;
+        x5 = xOffset + this.props.x5;
+        y5 = yOffset + this.props.y5;
+        x6 = xOffset + this.props.x6;
+        y6 = yOffset + this.props.y6;
+
+        let coords = "";
+
+        let pts = coords + x1 + "," + y1 + "," + x2 + "," + y2 + "," + x3 + "," + y3 + "," + x4 + "," + y4 + "," + x5 + "," + y5 + "," + x6 + "," + y6;
+
+        return <polygon points={pts} style={{ fillOpacity: '0.0', stroke: 'black', strokeWidth: '1' }} />;
+    }
+}
