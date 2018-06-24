759e5c43900aa78757a138825bdf862e01af22eb
Dzuke <Dzuke911@gmail.com>
2018-06-24 23:23:18 +0300

    123

---
+++ WoH_classes/BasicClasses/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WoH_classes.Managers;
+
+namespace WoH_classes.BasicClasses
+{
+    public class Game
+    {
+        private readonly UnitsManager _unitsManager;
+        private readonly UnitTypesManager _utm;
+        private readonly UnitTypeAttributesManager utam;
+    }
+}
