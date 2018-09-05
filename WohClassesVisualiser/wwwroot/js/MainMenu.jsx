75688ce6b3a108bee4c78c3cf196d77d23a0975b
Dzuke <Dzuke911@gmail.com>
2018-09-05 23:46:13 +0300

    123

--- WohClassesVisualiser/wwwroot/js/MainMenu.jsx
+++ WohClassesVisualiser/wwwroot/js/MainMenu.jsx
@@ -4,15 +4,8 @@ class MainMenu extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { accountData: this.props.accountData };
 
         this.TutorialClick = this.TutorialClick.bind(this);
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        if (this.props.accountData != nextProps.accountData) {
-            this.setState({ accountData: nextProps.accountData });
-        }
     }
 
     TutorialClick() {
@@ -22,7 +15,7 @@ class MainMenu extends React.Component {
     render() {
         return <div id="MainMenu" style={{ margin: 'auto', width: '600px', border: '3px solid blue', borderRadius: '30px 15px', padding: '10px' }} >
             <div style={{ margin: 'auto', width: '400px' }}>
-                {(this.state.accountData.TutorialComplete != true) && < button id="MainMenu_TutorialBtn" className="btn btn-primary" style={{ width : '100%' }} name="Tutorial" value="Tutorial" onClick={this.TutorialClick}><h3>Tutorial</h3></button>}
+                {(this.props.accountData.TutorialComplete != true) && < button id="MainMenu_TutorialBtn" className="btn btn-primary" style={{ width : '100%' }} name="Tutorial" value="Tutorial" onClick={this.TutorialClick}><h3>Tutorial</h3></button>}
             </div>
         </div>;
     }
